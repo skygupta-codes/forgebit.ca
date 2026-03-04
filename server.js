2442df522a938dbea1391e5d1656860642ee0e6b
Akash Gupta <akashgupta@c6:84:51:4c:ef:1e.home>
2026-03-04 17:47:43 -0500

    Bind Express listener to 0.0.0.0 for Hostinger App Hosting routing

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Forgebit server running on port ${PORT}`);
 });
