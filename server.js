7cbc4ee14701ce4ecdd8451ffcebab298008e5c6
Akash Gupta <akashgupta@c6:84:51:4c:ef:1e.home>
2026-03-04 18:07:50 -0500

    Remove Express 5 incompatible wildcard route causing 503 crash on boot

--- server.js
+++ server.js
@@ -36,11 +36,6 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-// Catch-all route to serve index.html for any other requests (SPA support)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Forgebit server running on port ${PORT}`);
 });
